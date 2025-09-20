e7ac4b7e2b62cb847f261cd8178cfc81eab74e42
AishaShaikh2009 <aisha.shaikh0509@gmail.com>
2025-09-20 11:45:04 +0400

    Add files via upload

---
+++ typesofdata.py
@@ -0,0 +1,8 @@
+a=98
+print("the type of a is: ", type(a))
+b=True
+print("the type of b is: ", type (b))
+c="Aisha"
+print("the type of c is: ", type(c))
+d=78.9
+print("the type of d is: ", type(d))
