ad99cdf41eeb98aba933718fb7ccf487f5641dfa
AishaShaikh2009 <aisha.shaikh0509@gmail.com>
2025-09-20 11:56:30 +0400

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,24 @@
+name="Aisha"
+age=16
+is_student=True
+weight=55.7
+
+print("Name: ", name)
+print("The type of name is:", type(name))
+
+print("Age: ", age)
+print("The type of age is: ", type(age))
+
+print("Is student: ", is_student)
+print("The type of data is: ", type(is_student))
+
+print("Weight: ", weight)
+print("The type of weight is: ", type(weight))
+
+age=str(age)
+print(age)
+print("The data type of age is: ", type(age))
+
+weight=int(weight)
+print(weight)
+print("The data type of weight is: ", type(weight))
